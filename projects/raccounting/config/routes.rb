728b40120b780bf79780b63b6cc9b6f8ec4a0b76
Uğur Aldanmaz <tool-accounts@jooweb.net>
2017-01-16 03:22:22 +0300

    Home controller added

--- projects/raccounting/config/routes.rb
+++ projects/raccounting/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  resources :company
+  resources :companies
   resources :customers
   resources :products
 
+  root 'home#index'
   get 'customers/index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
