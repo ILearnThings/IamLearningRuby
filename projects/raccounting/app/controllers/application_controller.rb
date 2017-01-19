eb1f45df994df9aa66e9632f59379d008e250328
Uğur Aldanmaz <tool-accounts@jooweb.net>
2017-01-19 22:47:49 +0300

    authenticate_user added to ApplicationController
    Warn! It is just a test. I do not know Devise yet. I will refactor here.

--- projects/raccounting/app/controllers/application_controller.rb
+++ projects/raccounting/app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user! # except: [:login]
+  before_action :validate_company_ownership #, :unless => :authenticate_user
+  # skip_before_action :current_company
+  #before_action :current_company #, :unless => :authenticate_user
 
+  private
+
+  def validate_company_ownership
+    if user_signed_in?
+      @company = Company.find(current_user.company.id)
+      
+      if @company.id.to_s != params[:company_id].to_s
+        redirect_to company_url(current_user.company.id)
+      end
+    else
+      authenticate_user!
+    end
+  
+      #end
+    #a = current_user.name
+    # if  current_user.company_id.to_s != params[:company_id].to_s
+    #   redirect_to root_path #unauthorized access, because this product is owned by another company
+    # end
+  end
 end
