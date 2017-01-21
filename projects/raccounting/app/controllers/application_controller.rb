e7bbfe01f789e30b613f2fb344935127d0d86dda
Uğur Aldanmaz <tool-accounts@jooweb.net>
2017-01-21 21:26:32 +0300

    Company authorization metod refactored

--- projects/raccounting/app/controllers/application_controller.rb
+++ projects/raccounting/app/controllers/application_controller.rb
@@ -1,32 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user! # except: [:login]
-  before_action :validate_company_ownership #, :unless => :authenticate_user
-  # skip_before_action :current_company
-  #before_action :current_company #, :unless => :authenticate_user
+  before_action :authenticate_user!
+  before_action :authorize_company
   layout :layout_by_resource
 
- private
+private
 
-  def validate_company_ownership
-    if user_signed_in?
+  def authorize_company
+    if !devise_controller?      
       @company = Company.find(current_user.company.id)
-      
-      if @company.id.to_s != params[:company_id].to_s
-        redirect_to company_url(current_user.company.id)
-      end
-    else
-      authenticate_user!
+      redirect_to company_url(current_user.company.id) unless @company.id.to_s == params[:company_id].to_s 
+      #unauthorized access, because company id is different then logined company
     end
-  
-      #end
-    #a = current_user.name
-    # if  current_user.company_id.to_s != params[:company_id].to_s
-    #   redirect_to root_path #unauthorized access, because this product is owned by another company
-    # end
   end
 
-   def layout_by_resource
+  def layout_by_resource
     if devise_controller?
       "devise"
     else
--- projects/raccounting/app/controllers/companies_controller.rb
+++ projects/raccounting/app/controllers/companies_controller.rb
@@ -2,14 +2,10 @@ class CompaniesController < ApplicationController
     def show
     end
 
-    private
-    
-    def validate_company_ownership
-        authenticate_user!
-        @company = Company.find(current_user.company.id)
+private
 
-        if @company.id.to_s != params[:id].to_s
-            redirect_to @company
-        end
+    def authorize_company
+        @company = Company.find(current_user.company.id)
+        redirect_to @company unless @company.id.to_s == params[:id].to_s
     end
 end
